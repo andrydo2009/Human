b86b2ef55add24b0968a181affb45653e2c348a5
Андрей <andrydo2009@yandex.ru>
2022-12-13 00:16:54 +0300

    Hello!

---
+++ src/Human.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class Human {
+
+    int yearOfBirth;       // год рождения
+    String name;          // имя
+    String town;          // город
+    String jobTitle;     // профессия
+
+    Human()
+    {}
+
+
+
+    Human (int yearOfBirth,String name,String town,String jobTitle) {
+        this.yearOfBirth = Math.max(yearOfBirth, 0);
+        if (name==null^ Objects.equals(name, "")){this.name="Информации нет";}
+        else {this.name=name;}
+        if (town==null^Objects.equals(town,"")){this.town="Информации нет";}
+        else {this.town=town;}
+        if (jobTitle==null^Objects.equals(jobTitle,"")){this.jobTitle="Информации нет";}
+        else {this.jobTitle=jobTitle;}
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " +
+                town + ". Я родился в " + yearOfBirth + " году"
+                + ". Я работаю на должности " + jobTitle + ". Будем знакомы!";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Human Person_1 = new Human(1990, "Аня", "Москва", "Фармацевт");
+        Human Person_2 = new Human(1978, "Максим", "Омск", "Крановщик");
+        Human Person_3 = new Human(2002, "Катя", "Казань", "Студентка");
+        Human Person_4 = new Human(2011, "Артем", "Хабаровск", "Школьник");
+        Human Person_5 = new Human(-80,"",null,"");
+        Human Person_6 = new Human();
+        System.out.println(Person_1);
+        System.out.println(Person_2);
+        System.out.println(Person_3);
+        System.out.println(Person_4);
+        System.out.println(Person_5);
+        System.out.println(Person_6);
+
+
+    }
+}
